e4bccdda1dfb9f28e46827fe6decf506ac56de05
Mark Karpeles <magicaltux@gmail.com>
2023-12-13 10:30:38 +0900

    add support for valueReader

--- offset.go
+++ offset.go
@@ -9,6 +9,10 @@ type offsetGetter interface {
 	OffsetGet(context.Context, string) (any, error)
 }
 
+type valueReader interface {
+	ReadValue(ctx context.Context) (any, error)
+}
+
 // OffsetGet returns v[offset] dealing with various case of figure. ctx will be passed to some methods handling it
 func OffsetGet(ctx context.Context, v any, offset string) (any, error) {
 	switch a := v.(type) {
@@ -27,6 +31,12 @@ func OffsetGet(ctx context.Context, v any, offset string) (any, error) {
 			return nil, nil
 		}
 		return a[n], nil
+	case valueReader: // keep this last
+		nv, err := a.ReadValue(ctx)
+		if err != nil {
+			return nil, err
+		}
+		return OffsetGet(ctx, nv, offset)
 	default:
 		return nil, fmt.Errorf("unsupported type %T for offset fetching", v)
 	}

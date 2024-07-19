1ac4cbfd3bcafd585311252b26878f7594bdf105
Mark Karpeles <magicaltux@gmail.com>
2024-07-20 02:20:12 +0900

    OffsetGet: add some support for specific cases and generic support via reflection

--- offset.go
+++ offset.go
@@ -3,6 +3,8 @@ package typutil
 import (
 	"context"
 	"fmt"
+	"net/url"
+	"reflect"
 )
 
 type offsetGetter interface {
@@ -20,6 +22,15 @@ func OffsetGet(ctx context.Context, v any, offset string) (any, error) {
 		return a.OffsetGet(ctx, offset)
 	case map[string]any:
 		return a[offset], nil
+	case map[string]string:
+		return a[offset], nil
+	case url.Values:
+		res := a[offset]
+		if len(res) == 0 {
+			return nil, nil
+		} else {
+			return res[0], nil
+		}
 	case []any:
 		// convert offset to int, ensure it is in range
 		n, ok := AsUint(offset)
@@ -38,6 +49,20 @@ func OffsetGet(ctx context.Context, v any, offset string) (any, error) {
 		}
 		return OffsetGet(ctx, nv, offset)
 	default:
+		vr := reflect.ValueOf(v)
+		switch vr.Kind() {
+		case reflect.Map:
+			switch vr.Type().Key().Kind() {
+			case reflect.String:
+				// this we can handle
+				v := vr.MapIndex(reflect.ValueOf(offset))
+				if v.IsZero() {
+					return nil, nil
+				} else {
+					return v.Interface(), nil
+				}
+			}
+		}
 		return nil, fmt.Errorf("unsupported type %T for offset fetching", v)
 	}
 }
